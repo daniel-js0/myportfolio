bb638339557387e581e03b0c47d8b77a2f9bec07
daniel-js0 <danielolagbegi1@gmail.com>
2023-11-03 16:46:15 +0100

    added an email link

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -22,36 +22,18 @@ const Contact = () => {
         <p className={`mt-6 px-5 md:px-1 text-lg text-slate-900 font-semibold dark:text-slate-100  md:text-xl  text-center
         delay-200 duration-500 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}>Do you have an Idea about anything? or want to bring your idea or presence to the web?. 
             Have a chat with me about your idea via any of the links below or send me an email by 
-            filling the form below. My inbox is always open.</p>
+            Clicking the Link below. My inbox is always open.</p>
         
-        <input type="text" 
-        className={`w-[100%] mx-auto h-16 focus:outline-blue-500 placeholder:text-slate-100 placeholder:text-lg px-5  rounded bg-neutral-500  mt-14
-        delay-300 duration-700 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}
-        placeholder="what's your name?"
-        />
-
-        <input type="email" 
-        className={`w-[100%] h-16 focus:outline-blue-500 placeholder:text-slate-100 placeholder:text-lg px-5 rounded bg-neutral-500 mt-6
-        delay-300 duration-700 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}
-        placeholder="what's your @email address?"
-        />
-
-        <input type="text" 
-        className={`w-[100%] h-16 focus:outline-blue-500 placeholder:text-slate-100 placeholder:text-lg px-5 rounded bg-neutral-500 mt-6
-        delay-300 duration-700 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}
-        placeholder="what do you have for me/ offer?"
-        />
+       
+        
 
         <a href="mailto:danielolagbegi1@gmail.com"> 
-          hello
+          <button className={`w-[100%] h-16 bg-blue-800 rounded-lg mt-7 text-xl font-bold text-slate-200
+          delay-300 duration-700 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}>
+                send it to me !
+          </button>
         </a>
-
-        
-        <button className={`w-[100%] h-16 bg-blue-800 rounded-lg mt-7 text-xl font-bold text-slate-200
-        delay-300 duration-700 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}>
-              send it to me !
-        </button>
-       
+ 
 
       </div>
       <Footer/>
