88c94f494bfaa0183bdfcebe24b5d1d9b2883891
daniel-js0 <danielolagbegi1@gmail.com>
2023-11-10 16:46:54 +0100

    updated

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -15,9 +15,9 @@ const Contact = () => {
   return (
   <main>
       <Navbar/>
-      <div className='mt-32 mb-28 h-[37rem] w-[90%] md:w-[65%] lg:w-[40%] bg-slate-00 mx-auto'>
+      <div className=' min-h-screen w-[90%] md:w-[65%] lg:w-[40%] bg-slate-00 mx-auto flex flex-col justify-center'>
 
-        <h1 className={`text-center  mt-32 text-blue-800 font-sans text-4xl tracking-wider md:text-5xl font-extrabold md:tracking-wide
+        <h1 className={`text-center  mt- text-blue-800 font-sans text-4xl tracking-wider md:text-5xl font-extrabold md:tracking-wide
         delay-100 duration-300 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}>Get In Touch</h1>
         <p className={`mt-6 px-5 md:px-1 text-lg text-slate-900 font-semibold dark:text-slate-100  md:text-xl  text-center
         delay-200 duration-500 ${isVisible ? 'translate-y-0 opacity-100' : ' translate-y-12 opacity-0'}`}>Do you have an Idea about anything? or want to bring your idea or presence to the web?. 
